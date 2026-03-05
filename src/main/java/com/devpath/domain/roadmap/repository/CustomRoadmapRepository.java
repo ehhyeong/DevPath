e257bfaa7eeab9b42111dd7f11031c341c030a6a
김태형 <jodiw571@naver.com>
2026-03-05 22:33:12 +0900

    feat: 커스텀 로드맵 선행조건 엔티티 및 Repository 구축

---
+++ src/main/java/com/devpath/domain/roadmap/repository/CustomRoadmapRepository.java
@@ -0,0 +1,11 @@
+package com.devpath.domain.roadmap.repository;
+
+import com.devpath.domain.roadmap.entity.CustomRoadmap;
+import com.devpath.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CustomRoadmapRepository extends JpaRepository<CustomRoadmap, Long> {
+    List<CustomRoadmap> findAllByUserOrderByCreatedAtDesc(User user);
+}
