0291ebc0b7e2b9af47ca3f567cb973bc62824d7b
ParkJus <bjs757800@gmail.com>
2026-03-07 21:53:58 +0900

    feat: 태그 및 오피셜 로드맵 관련 JPA 엔티티와 레포지토리 초기 설계

--- src/main/java/com/devpath/api/user/repository/TagRepository.java
+++ src/main/java/com/devpath/api/user/repository/TagRepository.java
@@ -2,6 +2,10 @@ package com.devpath.api.user.repository;
 
 import com.devpath.domain.user.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.List;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByName(String name);
+    List<Tag> findAllByIsOfficialTrue();
 }
